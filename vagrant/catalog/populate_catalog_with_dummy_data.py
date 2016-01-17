9a14cb4e009b8b9bd962200e57ef3734ed162c53
Subhash Guttikonda <subgutti@gmail.com>
2016-01-17 08:31:50 -0800

    Format the files

--- vagrant/catalog/populate_catalog_with_dummy_data.py
+++ vagrant/catalog/populate_catalog_with_dummy_data.py
@@ -20,7 +20,8 @@ session = DBSession()
 
 # Add dummy user
 User1 = User(name="Subhash Guttikonda", email="subgutti@gmail.com",
-             picture='https://lh6.googleusercontent.com/-xu4mYMJ8J7c/AAAAAAAAAAI/AAAAAAAANDg/_R-09t8IVhE/photo.jpg')
+             picture='https://lh6.googleusercontent.com/-xu4mYMJ8J7c'
+             '/AAAAAAAAAAI/AAAAAAAANDg/_R-09t8IVhE/photo.jpg')
 session.add(User1)
 session.commit()
 
@@ -31,19 +32,25 @@ category1 = Category(user_id=1, name="Soccer")
 session.add(category1)
 session.commit()
 
-item1 = Item(title="Soccer Socks", description="Soccer socks are extremely long. They cover shin-gaurds",
+item1 = Item(title="Soccer Socks", description="Soccer socks are "
+             "extremely long. They cover shin - gaurds",
              category=category1, user_id=1)
 session.add(item1)
 session.commit()
 
-item2 = Item(title="Shin-Gaurds", description="Shin-gaurds protect player's shins, a vulnerable part of player's body that often gets kicked",
+item2 = Item(title="Shin-Gaurds", description="Shin-gaurds protect player's "
+             "shins, a vulnerable part of player's body that often"
+             " gets kicked",
              category=category1, user_id=1)
 session.add(item2)
 session.commit()
 
 
-item3 = Item(title="Soccer Ball", description="Soccer balls allows players to train and play individually or with friends outside for practise",
-             category=category1, user_id=1, image="https://upload.wikimedia.org/wikipedia/en/e/ec/Soccer_ball.svg")
+item3 = Item(title="Soccer Ball", description="Soccer balls allows players to"
+             " train and play individually or with friends outside for "
+             "practise",
+             category=category1, user_id=1,
+             image="https://upload.wikimedia.org/wikipedia/en/e/ec/Soccer_ball.svg")
 session.add(item3)
 session.commit()
 
@@ -52,13 +59,17 @@ category2 = Category(user_id=1, name="Basketball")
 session.add(category2)
 session.commit()
 
-item4 = Item(title="Basketball Sneakers", description="Players should wear comfortable, properly fitting basketball sneakers.",
-             category=category2, user_id=1, image="http://ecx.images-amazon.com/images/I/91%2BXlOzjTwL._UX695_.jpg")
+item4 = Item(title="Basketball Sneakers", description="Players should wear "
+             "comfortable, properly fitting basketball sneakers.",
+             category=category2, user_id=1,
+             image="http://ecx.images-amazon.com/images/I/91%2BXlOzjTwL._UX695_.jpg")
 session.add(item4)
 session.commit()
 
-item5 = Item(title="Knee Pads", description="Players should wear knee pads to protect themselves during falls or dives to the floor",
-             category=category2, user_id=1, image="http://ecx.images-amazon.com/images/I/41j6DLiGSJL.jpg")
+item5 = Item(title="Knee Pads", description="Players should wear knee pads "
+             "to protect themselves during falls or dives to the floor",
+             category=category2, user_id=1,
+             image="http://ecx.images-amazon.com/images/I/41j6DLiGSJL.jpg")
 session.add(item5)
 session.commit()
 
@@ -67,13 +78,22 @@ category3 = Category(user_id=1, name="Baseball")
 session.add(category3)
 session.commit()
 
-item6 = Item(title="Baseball Glove", description="The quality of baseball gloves varies based mostly on the material (usually leather) that it's made out of. There are some very high end leather gloves on the market that are north of $500! These gloves are used mostly by professional players.",
-             category=category3, user_id=1, image="http://ecx.images-amazon.com/images/I/51VqFOSQKXL.jpg")
+item6 = Item(title="Baseball Glove", description="The quality of baseball "
+             "gloves varies based mostly on the material (usually leather) "
+             "that it's made out of. There are some very high end leather "
+             "gloves on the market that are north of $500! These gloves are "
+             "used mostly by professional players.",
+             category=category3, user_id=1,
+             image="http://ecx.images-amazon.com/images/I/51VqFOSQKXL.jpg")
 session.add(item6)
 session.commit()
 
-item7 = Item(title="Baseball Bat", description="You can't really play baseball without a bat! While there is a ton of fun to be had by just getting your glove on and playing catch with a friend, the game itself involves hitting and that means a bat.",
-             category=category3, user_id=1, image="http://ecx.images-amazon.com/images/I/71hNsLWogPL._SX522_.jpg")
+item7 = Item(title="Baseball Bat", description="You can't really play baseball"
+             " without a bat! While there is a ton of fun to be had by just "
+             "getting your glove on and playing catch with a friend, the game "
+             "itself involves hitting and that means a bat.",
+             category=category3, user_id=1,
+             image="http://ecx.images-amazon.com/images/I/71hNsLWogPL._SX522_.jpg")
 session.add(item7)
 session.commit()
 
--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, url_for, request, redirect, flash, jsonify
+from flask import Flask, render_template, url_for, request, flash
+from flask import make_response, redirect, jsonify
 from sqlalchemy import create_engine, asc, desc, and_
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
 from flask import session as login_session
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
-from flask import make_response
 
 import random
 import string
@@ -25,8 +25,14 @@ session = DBSession()
 
 
 # JSON APIs to view Catalog Information
+
 @app.route('/catalog/JSON')
 def catalogJSON():
+    """Returns the json response of the entire catalog
+
+    Returns:
+        JSON response of entire catalog
+    """
     categories = session.query(Category).all()
     response = []
     for category in categories:
@@ -41,6 +47,14 @@ def catalogJSON():
 
 @app.route('/catalog/<int:category_id>/JSON')
 def categoryJSON(category_id):
+    """Returns the json response of a particular category in the catalog
+
+    Args:
+      category_id: The category id
+
+    Returns:
+        JSON response of items in a category_id
+    """
     items = session.query(Item).filter_by(category_id=category_id).all()
     if items:
         return jsonify(Item=[i.serialize for i in items])
@@ -50,6 +64,15 @@ def categoryJSON(category_id):
 
 @app.route('/catalog/<int:category_id>/<int:item_id>/JSON')
 def itemJSON(category_id, item_id):
+    """Returns the json response of a particular item in a category
+
+    Args:
+      category_id: The catalog category id
+      item_id: The catalog item id
+
+    Returns:
+        JSON response of item description
+    """
     item = session.query(Item).filter_by(
         id=item_id).filter_by(category_id=category_id).one()
     if item:
@@ -58,21 +81,32 @@ def itemJSON(category_id, item_id):
         return ('', 204)
 
 
-# User connect
-# Create anti-forgery state token
-
+# User connect and disconnect apis
 
 @app.route('/login')
 def showLogin():
+    """Display the login page for a user
+
+    Returns:
+        HTML page that allows user to login
+        either using Google or Facebook.
+    """
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
-    # return "The current session state is %s" % login_session['state']
     return render_template('login.html', STATE=state)
 
 
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
+    """Validate facebook login response and get required
+    user information. Creates a new user if user doesn't
+    exist.
+
+    Returns:
+        HTML page that confirms the user details and automatically
+        redirects to the catalog home page
+    """
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
@@ -84,8 +118,9 @@ def fbconnect():
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?'
+    'grant_type=fb_exchange_token&client_id=%s&client_secret=%s&'
+    'fb_exchange_token=%s' % (app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
@@ -97,8 +132,6 @@ def fbconnect():
     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
     data = json.loads(result)
     login_session['provider'] = 'facebook'
     login_session['username'] = data["name"]
@@ -112,7 +145,8 @@ def fbconnect():
     login_session['access_token'] = stored_token
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&'
+    'redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -132,7 +166,8 @@ def fbconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius: " \
+    "150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
 
     flash("Now logged in as %s" % login_session['username'])
     return output
@@ -140,6 +175,14 @@ def fbconnect():
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+    """Validate google login response and get required
+    user information. Creates a new user if user doesn't
+    exist.
+
+    Returns:
+        HTML page that confirms the user details and automatically
+        redirects to the catalog home page
+    """
     # Validate state token
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
@@ -191,8 +234,9 @@ def gconnect():
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(
+            json.dumps('Current user is already connected.'),
+            200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -210,7 +254,6 @@ def gconnect():
     login_session['username'] = data['name']
     login_session['picture'] = data['picture']
     login_session['email'] = data['email']
-    # ADD PROVIDER TO LOGIN SESSION
     login_session['provider'] = 'google'
 
     # see if user exists, if it doesn't make a new one
@@ -225,15 +268,23 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius: " \
+    "150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("you are now logged in as %s" % login_session['username'])
     print "done!"
     return output
 
-# User helper functions
-
 
 def createUser(login_session):
+    """Create a new user.
+
+    Args:
+      login_session: A tuples containing username,
+      email and picture
+
+    Returns:
+        New user id
+    """
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
     session.add(newUser)
@@ -243,11 +294,29 @@ def createUser(login_session):
 
 
 def getUserInfo(user_id):
+    """Get user information given a user id.
+
+    Args:
+      user_id: User id
+
+    Returns:
+        User object that provides information about
+        username, user id, email, picture
+    """
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
 
 def getUserID(email):
+    """Get user id given a user email.
+
+    Args:
+      email: user Email
+
+    Returns:
+        User id associated to the given email or
+        None if no associated email.
+    """
     try:
         user = session.query(User).filter_by(email=email).one()
         return user.id
@@ -255,11 +324,15 @@ def getUserID(email):
         return None
 
 
-# User disconnect - Revoke a current user's token and reset their login_session
-
-
 @app.route('/fbdisconnect')
 def fbdisconnect():
+    """Disconnect user by clearing out any facebook
+    related information.
+
+    Returns:
+        A confirmation message that user has been
+        logged out.
+    """
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
@@ -272,7 +345,13 @@ def fbdisconnect():
 
 @app.route('/gdisconnect')
 def gdisconnect():
-    # Only disconnect a connected user.
+    """Disconnect user by clearing out any Google
+    related information.
+
+    Returns:
+        A confirmation message that user has been
+        logged out.
+    """
     credentials = login_session.get('credentials')
     if credentials is None:
         response = make_response(
@@ -290,11 +369,16 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
 
-# Disconnect based on provider
-
 
 @app.route('/disconnect')
 def disconnect():
+    """Disconnect user by clearing out any Facebook or Google
+    related information.
+
+    Returns:
+        A confirmation message that user has been
+        logged out.
+    """
     if 'provider' in login_session:
         if login_session['provider'] == 'google':
             gdisconnect()
@@ -314,38 +398,69 @@ def disconnect():
         flash("You were not logged in")
         return redirect(url_for('showCatalog'))
 
-# Show catalog
+# Catalog related apis
 
 
 @app.route('/')
 @app.route('/catalog')
 def showCatalog():
+    """Display the entire catalog.
+
+    Returns:
+        HTML page that displays the full catalog.
+    """
     categories = session.query(Category).order_by(asc(Category.name))
     recentItems = session.query(Item).order_by(desc(Item.id)).limit(5)
     recentlyAddedItems = []
     for item in recentItems:
         category = session.query(Category).filter_by(id=item.category_id).one()
         recentlyAddedItems.append(
-            dict([('item_name', item.title), ('category_name', category.name)]))
+            dict([('item_name', item.title),
+                  ('category_name', category.name)]))
 
-    return render_template('catalog.html', categories=categories, recentlyAddedItems=recentlyAddedItems)
+    return render_template('catalog.html',
+                           categories=categories,
+                           recentlyAddedItems=recentlyAddedItems)
 
 
 @app.route('/<string:category_name>')
 def showCategoryItems(category_name):
+    """Display a particular catalog category items.
+
+    Args:
+      category_name: Category name for which items should be
+      displayed
+
+    Returns:
+        HTML page that displays the category items. If the requested
+        category doesn't exists then redirects to home page.
+    """
     categories = session.query(Category).order_by(asc(Category.name))
     currentCategory = session.query(
         Category).filter_by(name=category_name).first()
     if currentCategory:
         categoryItems = session.query(Item).filter_by(
             category_id=currentCategory.id).all()
-        return render_template('category.html', categories=categories, currentCategory=currentCategory, categoryItems=categoryItems)
+        return render_template('category.html',
+                               categories=categories,
+                               currentCategory=currentCategory,
+                               categoryItems=categoryItems)
     else:
         return redirect(url_for('showCatalog'))
 
 
 @app.route('/<string:category_name>/<string:item_name>')
 def showItem(category_name, item_name):
+    """Display a particular item information
+
+    Args:
+      category_name: Category name the item belongs to
+      item_name: Item name to display information
+
+    Returns:
+        HTML page that displays the item information. If the requested
+        item doesn't exists then redirects to home page.
+    """
     selectedCategory = session.query(
         Category).filter_by(name=category_name).first()
     if selectedCategory:
@@ -353,18 +468,31 @@ def showItem(category_name, item_name):
             category_id=selectedCategory.id).all()
         selectedItem = session.query(Item).filter_by(
             title=item_name).filter_by(category_id=selectedCategory.id).one()
-        return render_template('item.html', selectedCategory=selectedCategory, selectedCategoryItems=selectedCategoryItems, selectedItem=selectedItem)
+        return render_template('item.html',
+                               selectedCategory=selectedCategory,
+                               selectedCategoryItems=selectedCategoryItems,
+                               selectedItem=selectedItem)
     else:
         return redirect(url_for('showCatalog'))
 
 
 @app.route('/catalog/item/add', methods=['GET', 'POST'])
 def addItem():
+    """Display a HTML page that allows user to add a new item.
+    Also save the newly created item in database.
+
+    Returns:
+        HTML page that allows user to create new item. If user
+        is not logged in, then redirects to login page.
+    """
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
-        newItem = Item(title=request.form['name'], description=request.form[
-            'description'], image=request.form['image'], category_id=request.form['category'], user_id=login_session['user_id'])
+        newItem = Item(title=request.form['name'],
+                       description=request.form['description'],
+                       image=request.form['image'],
+                       category_id=request.form['category'],
+                       user_id=login_session['user_id'])
         session.add(newItem)
         session.commit()
         flash('Item successfully created : %s' % (newItem.title))
@@ -376,13 +504,27 @@ def addItem():
 
 @app.route('/catalog/<string:item_name>/edit', methods=['GET', 'POST'])
 def editItem(item_name):
+    """Display a HTML page that allows user to edit an item.
+    Also save the newly edit item in database.
+
+    Args:
+      item_name: Item name to edit
+
+    Returns:
+        HTML page that allows user to edit an item.
+        If user is not logged in, then redirects to login page.
+        If user is not authorized to edit, displays a warning message.
+    """
     if 'username' not in login_session:
         return redirect(url_for('showLogin'))
     itemToEdit = session.query(Item).filter_by(title=item_name).first()
     if not itemToEdit:
         return redirect(url_for('showCatalog'))
     if login_session['user_id'] != itemToEdit.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to edit this item. Please create your own item in order to edit.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction() {alert('You are not " \
+            "authorized to edit this item. Please create your own " \
+            "item in order to edit.');}" \
+            "</script><body onload='myFunction()''>"
     if request.method == 'POST':
         if request.form['name']:
             itemToEdit.title = request.form['name']
@@ -398,18 +540,34 @@ def editItem(item_name):
         return redirect(url_for('showCatalog'))
     else:
         categories = session.query(Category).order_by(asc(Category.name))
-        return render_template('edititem.html', item=itemToEdit, categories=categories)
+        return render_template('edititem.html',
+                               item=itemToEdit,
+                               categories=categories)
 
 
 @app.route('/catalog/<string:item_name>/delete', methods=['GET', 'POST'])
 def deleteItem(item_name):
+    """Display a HTML page that allows user to delete an item.
+    Also delete the item in database.
+
+    Args:
+      item_name: Item name to delete
+
+    Returns:
+        HTML page that allows user to delete an item.
+        If user is not logged in, then redirects to login page.
+        If user is not authorized to delete, displays a warning message.
+    """
     if 'username' not in login_session:
         return redirect(url_for('showLogin'))
     itemToDelete = session.query(Item).filter_by(title=item_name).first()
     if not itemToDelete:
         return redirect(url_for('showCatalog'))
     if login_session['user_id'] != itemToDelete.user_id:
-        return "<script>function myFunction() {alert('You are not authorized to delete this item. Please create your own item in order to delete.');}</script><body onload='myFunction()''>"
+        return "<script>function myFunction() {alert('You are not " \
+            "authorized to delete this item. Please create your own " \
+            " item in order to delete.');}" \
+            "</script><body onload='myFunction()''>"
     if request.method == 'POST':
         session.delete(itemToDelete)
         session.commit()
@@ -421,10 +579,22 @@ def deleteItem(item_name):
 
 @app.context_processor
 def override_url_for():
+    """Override static folder urls to include timestamp. So that
+    the modified static files are reflected immediately to the user.
+
+    Returns:
+        Modified URL for static files with timestamp
+    """
     return dict(url_for=dated_url_for)
 
 
 def dated_url_for(endpoint, **values):
+    """Override static folder urls to include timestamp. So that
+    the modified static files are reflected immediately to the user.
+
+    Returns:
+        Modified URL for static files with timestamp
+    """
     if endpoint == 'static':
         filename = values.get('filename', None)
         if filename:
